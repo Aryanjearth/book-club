3e3fe858157c3d3acc3845fb439877b383adcc2d
Aryanjearth <aryanjearth07@gmail.com>
2025-09-04 14:39:30 +0530

    changed mongoDb uri

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME : "book_worms",
-    DB_URL : "mongodb://0.0.0.0/book_worms"
+    DB_URL : "mongodb+srv://aryan:Aarjearth777@cluster0.yfrdu.mongodb.net/bookClub?retryWrites=true&w=majority&appName=Cluster0"
 }
